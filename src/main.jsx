774c58fa3d648b2b3b665ebe4e34692b83d50815
Jaime Pineda <jpin730@pm.me>
2023-05-31 12:24:13 -0600

    build: :rocket: deploy app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { JournalApp } from "./JournalApp";
-import "./styles";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
