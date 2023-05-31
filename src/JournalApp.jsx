eb9d8387d4030f2ca90449127fdbc70845e6ec28
Jaime Pineda <jpin730@pm.me>
2023-05-31 14:33:06 -0600

    style: :lipstick: add material ui

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,11 +1,14 @@
 import { HashRouter } from "react-router-dom";
 
 import { AppRouter } from "./router";
+import { AppTheme } from "./theme";
 
 export const JournalApp = () => {
   return (
     <HashRouter>
-      <AppRouter />
+      <AppTheme>
+        <AppRouter />
+      </AppTheme>
     </HashRouter>
   );
 };
