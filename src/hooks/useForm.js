e5360e3c6bc7f8d3bf5a607663243e326d336fa3
Jaime Pineda <jpin730@pm.me>
2023-06-06 15:00:52 -0600

    feat: :sparkles: add validations to use form hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialForm);
+  const [formErrorsState, setFormErrorsState] = useState({});
+
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
+
+  const isFormValid = useMemo(() => {
+    for (const fieldError of Object.values(formErrorsState)) {
+      if (fieldError !== null) return false;
+    }
+
+    return true;
+  }, [formErrorsState]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -15,9 +28,27 @@ export const useForm = (initialForm = {}) => {
     setFormState(initialForm);
   };
 
+  const createValidators = () => {
+    const formCheckedValues = {};
+
+    for (const [fieldName, [validationFn, errorMessage]] of Object.entries(
+      formValidations
+    )) {
+      formCheckedValues[`${fieldName}Error`] = validationFn(
+        formState[fieldName]
+      )
+        ? null
+        : errorMessage;
+    }
+
+    setFormErrorsState(formCheckedValues);
+  };
+
   return {
     ...formState,
+    ...formErrorsState,
     formState,
+    isFormValid,
     onInputChange,
     onResetForm,
   };
