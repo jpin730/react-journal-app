9e7635f873ea6c8b6d38e9e8621f51d710d87970
Jaime Pineda <jpin730@pm.me>
2023-06-06 15:01:17 -0600

    feat: :sparkles: add validations to register page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,43 +1,99 @@
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
 
 import { AuthLayout } from "../layouts";
+import { useForm } from "../../hooks";
+
+const formData = {
+  email: "",
+  password: "",
+  displayName: "",
+};
+
+const formValidations = {
+  email: [(value) => value.includes("@"), "Email must have an '@'."],
+  password: [
+    (value) => value.length >= 6,
+    "Password must be 6 letters or more.",
+  ],
+  displayName: [(value) => value.length > 0, "Username is required."],
+};
 
 export const RegisterPage = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const {
+    displayName,
+    email,
+    password,
+    displayNameError,
+    emailError,
+    passwordError,
+    isFormValid,
+    onInputChange,
+  } = useForm(formData, formValidations);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    console.log(displayName, email, password);
+  };
+
   return (
     <AuthLayout title="Create an account">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
-              label="Full name"
+              fullWidth
               type="text"
-              placeholder="Enter your full name"
-              fullWidth
+              name="displayName"
+              label="Username"
+              placeholder="Enter your username"
+              value={displayName}
+              error={!!displayNameError && formSubmitted}
+              helperText={displayNameError}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
-              label="Email"
+              fullWidth
               type="email"
+              name="email"
+              label="Email"
               placeholder="Enter your email"
-              fullWidth
+              value={email}
+              error={!!emailError && formSubmitted}
+              helperText={emailError}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
-              label="Password"
-              type="password"
-              placeholder="Enter your password"
               fullWidth
+              type="password"
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              value={password}
+              error={!!passwordError && formSubmitted}
+              helperText={passwordError}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button
+                fullWidth
+                variant="contained"
+                type="submit"
+                disabled={!isFormValid}
+              >
                 Create
               </Button>
             </Grid>
